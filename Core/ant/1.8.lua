a597868605142b13ccb9ebdda3126aa201309952
Adam Caprez <acaprez2@unl.edu>
2015-08-05 11:24:10 -0500

    Add module file for Apache Ant

---
+++ Core/ant/1.8.lua
@@ -0,0 +1,16 @@
+help (
+[[
+This module loads ant
+
+Version 1.8
+]])
+
+whatis("Name: gnuplot")
+whatis("Version: 4.6")
+whatis("Category: Utility, Java")
+whatis("Keywords:  Utility, Java")
+whatis("Description: Apache Ant is a Java library and command-line tool whose mission is to drive processes described in build files as targets and extension points dependent upon each other.")
+whatis("URL: http://ant.apache.org")
+
+prepend_path("PATH"       ,"/util/opt/ant/1.8/bin")
+setenv("ANT_HOME"	  ,"/util/opt/ant/1.8")
